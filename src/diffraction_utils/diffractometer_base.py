6b9d50be72a506c1bdf6f5bd681c4c49fab7116e
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-05-04 15:45:27 +0100

    Made ABC for Diffractometers

--- src/diffraction_utils/diffractometer_base.py
+++ src/diffraction_utils/diffractometer_base.py
@@ -2,10 +2,16 @@
 This module contains the DiffractometerBase class.
 """
 
+from abc import ABC, abstractmethod
+
+from scipy.spatial.transform import Rotation
+
+from .frame_of_reference import Frame
 from .io import NexusBase
+from .vector import Vector3
 
 
-class DiffractometerBase:
+class DiffractometerBase(ABC):
     """
     This contains a generic description of what all diffractometers need to
     have.
@@ -13,3 +19,38 @@ class DiffractometerBase:
 
     def __init__(self, nexus: NexusBase) -> None:
         self.nexus = nexus
+
+    @property
+    @abstractmethod
+    def u_matrix(self) -> Rotation:
+        """
+        The Rotation from of the so-called "U" rotation matrix.
+        """
+
+    @property
+    @abstractmethod
+    def b_matrix(self) -> Rotation:
+        """
+        The Rotation form of the so-called "B" rotation matrix.
+        """
+
+    @property
+    @abstractmethod
+    def ub_matrix(self) -> Rotation:
+        """
+        The Rotation form of the so-called "UB" rotation matrix.
+        """
+
+    @property
+    @abstractmethod
+    def to_frame(self, vector: Vector3, frame: Frame) -> None:
+        """
+        Rotates the vector passed as an argument into the frame specified by the
+        frame argument.
+
+        Args:
+            vector:
+                The vector to rotate.
+            frame:
+                The frame into which the vector will be rotated.
+        """
