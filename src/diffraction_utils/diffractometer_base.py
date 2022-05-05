6de7963c637ee95f2e5830206f3da0fc3cc7b509
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-05-05 11:52:30 +0100

    Added functionality to DiffractometerBase and Frame

--- src/diffraction_utils/diffractometer_base.py
+++ src/diffraction_utils/diffractometer_base.py
@@ -9,7 +9,7 @@ from scipy.spatial.transform import Rotation
 
 from .frame_of_reference import Frame
 from .io import NexusBase
-from .vector import Vector3
+from .vector import Vector3, rot_from_a_to_b
 
 
 class DiffractometerBase(ABC):
@@ -20,32 +20,65 @@ class DiffractometerBase(ABC):
 
     def __init__(self, nexus: NexusBase, sample_oop: np.ndarray) -> None:
         self.nexus = nexus
-        self.sample_oop = np.array(sample_oop)
+        if not isinstance(sample_oop, Vector3):
+            sample_oop = np.array(sample_oop)
+            frame = Frame(Frame.sample_holder, self)
+            self.sample_oop = Vector3(sample_oop, frame)
 
-    @property
     @abstractmethod
-    def u_matrix(self) -> Rotation:
+    def get_u_matrix(self, scan_index: int) -> Rotation:
         """
-        The scipy Rotation from of the so-called "U" rotation matrix.
+        The scipy Rotation from of the so-called "U" rotation matrix. This must
+        be calculated in children of DiffractometerBase on a diffractometer-by-
+        diffractometer basis.
+
+        Args:
+            scan_index:
+                The U matrix generally varies throughout a scan. The scan_index
+                parameter specified which step of the scan we want to generate
+                a U matrix for.
+
+        Returns:
+            Instance of Rotation corresponding to the U matrix of interest.
         """
 
-    @property
-    @abstractmethod
-    def b_matrix(self) -> Rotation:
+    def get_b_matrix(self) -> Rotation:
         """
-        The scipy Rotation form of the so-called "B" rotation matrix.
-        """
+        The scipy Rotation form of the so-called "B" rotation matrix. This
+        matrix maps vectors from the reciprocal lattice's hkl frame to a
+        coordinate frame anchored to the sample holder. This could be made a
+        property, but is left as a method for symmetry with the U and UB
+        matrices.
 
-    @property
-    def ub_matrix(self) -> Rotation:
+        TODO: generalize so that this works for non-cubic crystals. This should
+            be implemented by making Vector3's sentient of their basis vectors,
+            and then modifying rot_from_a_to_b.
+
+        Returns:
+            Instance of Rotation corresponding to the B matrix for your sample.
+        """
+        # Generate a rotation from the sample_oop to the holder_oop
+        holder_oop = Vector3([0, 1, 0], Frame(Frame.sample_holder, self))
+        return rot_from_a_to_b(self.sample_oop, holder_oop)
+
+    def get_ub_matrix(self, scan_index: int) -> Rotation:
         """
         The scipy Rotation form of the so-called "UB" rotation matrix.
-        """
-        return self.u_matrix * self.b_matrix
 
-    @property
-    @abstractmethod
-    def to_frame(self, vector: Vector3, frame: Frame) -> None:
+        Args:
+            scan_index:
+                The UB matrix generally varies throughout a scan, as the motion
+                of the diffractometer motors affects the U matrix. The
+                scan_index parameter specified which step of the scan we want to
+                generate a U matrix (and therefore also the UB matrix) for.
+
+        Returns:
+            Instance of Rotation corresponding to the UB matrix for the
+            scan_index of interest.
+        """
+        return self.get_u_matrix(scan_index) * self.get_b_matrix()
+
+    def to_frame(self, vector: Vector3, to_frame: Frame) -> None:
         """
         Rotates the vector passed as an argument into the frame specified by the
         frame argument.
@@ -56,3 +89,34 @@ class DiffractometerBase(ABC):
             frame:
                 The frame into which the vector will be rotated.
         """
+        # Return the identity if no rotation is required.
+        if vector.frame.frame_name == to_frame.frame_name:
+            return Rotation.from_rotvec([0, 0, 0])
+
+        # Okay, we're changing frame. We have to handle each case individually.
+        match vector.frame.frame_name, to_frame.frame_name:
+            case Frame.lab, Frame.hkl:
+                # To go from the lab to hkl we need the inverse of UB.
+                return self.get_ub_matrix(vector.frame.scan_index).inv()
+            case Frame.lab, Frame.sample_holder:
+                # To go from the lab to the sample holder we just need U^-1.
+                return self.get_u_matrix(vector.frame.scan_index).inv()
+
+            case Frame.sample_holder, Frame.lab:
+                # We can use U to go from the sample holder to the lab.
+                return self.get_u_matrix(to_frame.scan_index)
+            case Frame.sample_holder, Frame.hkl:
+                # We can use B^-1 to go from the sample holder to hkl space.
+                return self.get_b_matrix().inv()
+
+            case Frame.hkl, Frame.lab:
+                # This is precisely what the UB matrix is for!
+                return self.get_ub_matrix(to_frame.scan_index)
+            case Frame.hkl, Frame.sample_holder:
+                # This is what defines the B matrix.
+                return self.get_b_matrix()
+
+            case _:
+                # Invalid frame name, raise an error
+                raise ValueError(
+                    "Tried to rotate to or from a frame with an invalid name.")
--- src/diffraction_utils/frame_of_reference.py
+++ src/diffraction_utils/frame_of_reference.py
@@ -14,14 +14,26 @@ class Frame:
     during a scan. Instead, the frame of reference is completely described by
     a combination of an index and a string identifier.
 
-    If it becomes necessary to generalise the concept of a frame in the future,
-    this abstraction will simplify the process.
+    It is worth noting that in several special cases, scan_index does not need
+    to be provided. For example, consider a crystal glued to a sample holder.
+    In the Frame.sample_holder frame, descriptions of the crystal are
+    independent of scan index. In that case, scan indices are only needed to
+    transform to or from this frame: they are not needed to describe vectors in
+    this frame.
+
+    On the flip side, a vector describing a property of the crystal in the lab
+    frame will need a scan_index and a diffractometer, since the diffractometer
+    is generally moving during a scan.
     """
+
+    sample_holder = 'sample holder'
+    hkl = 'hkl'
+    lab = 'lab'
 
     def __init__(self,
                  frame_name: str,
-                 scan_index: int,
-                 diffractometer: DiffractometerBase):
+                 diffractometer: DiffractometerBase = None,
+                 scan_index: int = None):
         self.frame_name = frame_name
-        self.scan_index = scan_index
         self.diffractometer = diffractometer
+        self.scan_index = scan_index
