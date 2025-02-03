dea3df1ee532d7e8483d05ef74f16e908e067345
Philip Mousley <philipmousley@hotmail.com>
2025-02-03 16:05:45 +0000

    edit parsing of scanlength and some motors

--- src/diffraction_utils/data_file.py
+++ src/diffraction_utils/data_file.py
@@ -143,7 +143,12 @@ class DataFileBase(ABC):
         """
         Returns the number of data points collected during this scan.
         """
-        return np.size(self.default_signal)
+        if len(np.shape(self.default_signal))==3:
+            return np.shape(self.default_signal)[0]
+        elif len(np.shape(self.default_signal))==4:
+            return np.shape(self.default_signal)[0]*np.shape(self.default_signal)[1]
+        else:
+            return np.size(self.default_signal)
 
     @abstractmethod
     def _parse_hdf5_internal_path(self) -> str:
