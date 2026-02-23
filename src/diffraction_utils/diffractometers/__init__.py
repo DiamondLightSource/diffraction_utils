e3ee89e5ace698fdb2ac581d60dc1aa1450a81e7
pmousley <philipmousley@hotmail.com>
2026-02-23 15:21:05 +0000

    remove unused i10 import

--- src/diffraction_utils/diffractometers/__init__.py
+++ src/diffraction_utils/diffractometers/__init__.py
@@ -3,5 +3,4 @@ Import some DiffractometerBase implementations directly to the diffractometers
 module.
 """
 
-from .diamond_i10 import I10RasorDiffractometer
 from .diamond_i07 import I07Diffractometer
